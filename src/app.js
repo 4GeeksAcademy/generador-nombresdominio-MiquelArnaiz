60134340167b615cfeb125b2cacb789764a75d69
Miquel Arnaiz Fayos <arnaizmiquel@gmail.com>
2026-03-27 20:38:38 +0000

    web ya funciona

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  
+  for (let pronoun1 = 0; pronoun1 < pronoun.length ; pronoun1++){
+     for (let adj1 = 0; adj1 < adj.length ; adj1++){
+       for (let noun1 = 0; noun1 < noun.length ; noun1++){
+    console.log(pronoun[pronoun1] + adj[adj1] + noun[noun1] + ".com");
+  }
+ }
+}
+ function web () {
+  let dominios =""
+pronoun.forEach((pronoun2) =>{
+  adj.forEach((adj2) => {
+    noun.forEach((noun2) => {
+      dominios += pronoun2 + adj2 + noun2 + ".com<br>"
+     
+    })
+   })
+  })
+return dominios
+}
+document.getElementById("nombre").innerHTML = web();
